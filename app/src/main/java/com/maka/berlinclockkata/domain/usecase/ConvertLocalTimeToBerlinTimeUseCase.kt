baf0b692c37e6df9fa9bc5d0fc67ad732be0528f
MhdAdelKasas <mhd.adel.kasas@gmail.com>
2026-02-09 15:07:21 +0100

    Convert to BerlinTime now correctly converts the seconds

--- app/src/main/java/com/maka/berlinclockkata/domain/usecase/ConvertLocalTimeToBerlinTimeUseCase.kt
+++ app/src/main/java/com/maka/berlinclockkata/domain/usecase/ConvertLocalTimeToBerlinTimeUseCase.kt
@@ -1,13 +1,14 @@
 package com.maka.berlinclockkata.domain.usecase
 
 import com.maka.berlinclockkata.domain.model.BerlinTime
+import com.maka.berlinclockkata.domain.model.LightBulbStatus
 import java.time.LocalTime
 
 class ConvertLocalTimeToBerlinTimeUseCase {
 
     operator fun invoke(time: LocalTime): BerlinTime =
         BerlinTime(
-
+            secondsLightBulb = if (time.second % 2 == 0) LightBulbStatus.ON else LightBulbStatus.OFF,
         )
 
 }
