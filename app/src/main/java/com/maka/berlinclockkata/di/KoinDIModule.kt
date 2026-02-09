cdd025eae94030c4f0a6f410710a01be2c1a166b
MhdAdelKasas <mhd.adel.kasas@gmail.com>
2026-02-09 14:16:06 +0100

    add time screen view model to koin

--- app/src/main/java/com/maka/berlinclockkata/di/KoinDIModule.kt
+++ app/src/main/java/com/maka/berlinclockkata/di/KoinDIModule.kt
@@ -1,7 +1,9 @@
 package com.maka.berlinclockkata.di
 
+import com.maka.berlinclockkata.ui.TimeScreenViewModel
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.module
 
 val appModule = module {
-
+    viewModelOf(::TimeScreenViewModel)
 }
