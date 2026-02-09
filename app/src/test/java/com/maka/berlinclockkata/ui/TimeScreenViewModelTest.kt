1f5b31d66e613660f8f03ee9d1d2ddcfc8cdce95
MhdAdelKasas <mhd.adel.kasas@gmail.com>
2026-02-09 13:33:09 +0100

    add check initstate test

--- app/src/test/java/com/maka/berlinclockkata/ui/TimeScreenViewModelTest.kt
+++ app/src/test/java/com/maka/berlinclockkata/ui/TimeScreenViewModelTest.kt
@@ -2,13 +2,18 @@ package com.maka.berlinclockkata.ui
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.advanceTimeBy
 import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
+import kotlin.test.assertNotEquals
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class TimeScreenViewModelTest {
@@ -25,6 +30,12 @@ class TimeScreenViewModelTest {
     @After
     fun tearDown() {
         Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `uiState initial value has empty system time`() {
+        val initialState = viewModel.uiState.value
+        assertEquals("", initialState.systemTime)
     }
 
     @Test
