3d5ace90349fdb0a5867abb7790f885c83985f37
MhdAdelKasas <mhd.adel.kasas@gmail.com>
2026-02-09 19:50:19 +0100

    add berlin time to ui state

--- app/src/main/java/com/maka/berlinclockkata/ui/UIState.kt
+++ app/src/main/java/com/maka/berlinclockkata/ui/UIState.kt
@@ -1,5 +1,8 @@
 package com.maka.berlinclockkata.ui
 
+import com.maka.berlinclockkata.domain.model.BerlinTime
+
 data class UIState(
+    val berlinTime: BerlinTime = BerlinTime(),
     val systemTime: String = ""
 )
