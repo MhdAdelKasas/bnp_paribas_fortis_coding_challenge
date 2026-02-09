e9ad9ec7a1a0fb9dc8594ef62562bd1c9f15b634
MhdAdelKasas <mhd.adel.kasas@gmail.com>
2026-02-09 19:49:43 +0100

    add tests for the berlin time in the ui state

--- app/src/test/java/com/maka/berlinclockkata/ui/TimeScreenViewModelTest.kt
+++ app/src/test/java/com/maka/berlinclockkata/ui/TimeScreenViewModelTest.kt
@@ -1,19 +1,17 @@
 package com.maka.berlinclockkata.ui
 
+import com.maka.berlinclockkata.domain.model.BerlinTime
+import com.maka.berlinclockkata.domain.model.LightBulbStatus
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
-import kotlinx.coroutines.test.advanceTimeBy
 import kotlinx.coroutines.test.resetMain
-import kotlinx.coroutines.test.runTest
 import kotlinx.coroutines.test.setMain
 import org.junit.After
 import org.junit.Assert.assertEquals
 import org.junit.Assert.assertNotNull
 import org.junit.Before
 import org.junit.Test
-import kotlin.test.assertNotEquals
 
 @OptIn(ExperimentalCoroutinesApi::class)
 class TimeScreenViewModelTest {
@@ -33,6 +31,12 @@ class TimeScreenViewModelTest {
     }
 
     @Test
+    fun `uiState initial value has default BerlinTime`() {
+        val initialState = viewModel.uiState.value
+        assertEquals(BerlinTime(), initialState.berlinTime)
+    }
+
+    @Test
     fun `uiState initial value has empty system time`() {
         val initialState = viewModel.uiState.value
         assertEquals("", initialState.systemTime)
@@ -43,5 +47,38 @@ class TimeScreenViewModelTest {
         assertNotNull(viewModel.uiState.value)
     }
 
+    @Test
+    fun `berlinTime in initial state has all lights off`() {
+        val berlinTime = viewModel.uiState.value.berlinTime
 
+        assertEquals(LightBulbStatus.OFF, berlinTime.secondsLightBulb)
+        assertEquals(4, berlinTime.fiveHoursLightBulbs.count { it == LightBulbStatus.OFF })
+        assertEquals(4, berlinTime.oneHoursLightBulbs.count { it == LightBulbStatus.OFF })
+        assertEquals(11, berlinTime.fiveMinutesLightBulbs.count { it == LightBulbStatus.OFF })
+        assertEquals(4, berlinTime.oneMinuteLightBulbs.count { it == LightBulbStatus.OFF })
+    }
+
+    @Test
+    fun `fiveHoursLightBulbs has 4 elements`() {
+        val size = viewModel.uiState.value.berlinTime.fiveHoursLightBulbs.size
+        assertEquals(4, size)
+    }
+
+    @Test
+    fun `oneHoursLightBulbs has 4 elements`() {
+        val size = viewModel.uiState.value.berlinTime.oneHoursLightBulbs.size
+        assertEquals(4, size)
+    }
+
+    @Test
+    fun `fiveMinutesLightBulbs has 11 elements`() {
+        val size = viewModel.uiState.value.berlinTime.fiveMinutesLightBulbs.size
+        assertEquals(11, size)
+    }
+
+    @Test
+    fun `oneMinuteLightBulbs has 4 elements`() {
+        val size = viewModel.uiState.value.berlinTime.oneMinuteLightBulbs.size
+        assertEquals(4, size)
+    }
 }
