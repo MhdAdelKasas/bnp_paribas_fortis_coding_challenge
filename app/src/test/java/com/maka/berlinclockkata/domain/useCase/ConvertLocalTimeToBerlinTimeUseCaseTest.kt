e328c79efa783ede8096bada7e258d8bab72c558
MhdAdelKasas <mhd.adel.kasas@gmail.com>
2026-02-09 14:54:49 +0100

    add seconds Light Bulb tests

---
+++ app/src/test/java/com/maka/berlinclockkata/domain/useCase/ConvertLocalTimeToBerlinTimeUseCaseTest.kt
@@ -0,0 +1,43 @@
+package com.maka.berlinclockkata.domain.useCase
+
+import com.maka.berlinclockkata.domain.model.LightBulbStatus
+import com.maka.berlinclockkata.domain.useCase.ConvertLocalTimeToBerlinTimeUseCase
+import org.junit.Before
+import org.junit.Test
+import java.time.LocalTime
+import kotlin.test.assertEquals
+
+class ConvertLocalTimeToBerlinTimeUseCaseTest {
+
+    private lateinit var useCase: ConvertLocalTimeToBerlinTimeUseCase
+
+    @Before
+    fun setUp() {
+        useCase = ConvertLocalTimeToBerlinTimeUseCase()
+    }
+
+    // Seconds Light Bulb Tests
+    @Test
+    fun `seconds light bulb is ON when seconds are even (0)`() {
+        val result = useCase(LocalTime.of(12, 30, 0))
+        assertEquals(LightBulbStatus.ON, result.secondsLightBulb)
+    }
+
+    @Test
+    fun `seconds light bulb is OFF when seconds are odd (1)`() {
+        val result = useCase(LocalTime.of(12, 30, 1))
+        assertEquals(LightBulbStatus.OFF, result.secondsLightBulb)
+    }
+
+    @Test
+    fun `seconds light bulb is ON when seconds are even (58)`() {
+        val result = useCase(LocalTime.of(12, 30, 58))
+        assertEquals(LightBulbStatus.ON, result.secondsLightBulb)
+    }
+
+    @Test
+    fun `seconds light bulb is OFF when seconds are odd (59)`() {
+        val result = useCase(LocalTime.of(12, 30, 59))
+        assertEquals(LightBulbStatus.OFF, result.secondsLightBulb)
+    }
+}
