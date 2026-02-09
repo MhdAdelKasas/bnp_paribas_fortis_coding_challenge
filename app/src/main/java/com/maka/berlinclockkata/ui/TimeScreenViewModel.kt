8a608581dc316aa410c1cc26bbdd667a2b98d856
MhdAdelKasas <mhd.adel.kasas@gmail.com>
2026-02-09 13:33:50 +0100

    read the device time and update the ui state with the format HH:mm:ss

--- app/src/main/java/com/maka/berlinclockkata/ui/TimeScreenViewModel.kt
+++ app/src/main/java/com/maka/berlinclockkata/ui/TimeScreenViewModel.kt
@@ -2,15 +2,33 @@ package com.maka.berlinclockkata.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
 
 class TimeScreenViewModel : ViewModel() {
 
-    val uiState: StateFlow<UIState> = flow<UIState> {
+    private val timeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss")
 
+    private val timeFlow = flow {
+        while (true) {
+            emit(LocalTime.now())
+            delay(1000)
+        }
+    }
+
+    val uiState: StateFlow<UIState> = flow<UIState> {
+        timeFlow.collect { time ->
+            emit(
+                UIState(
+                    systemTime = time.format(timeFormatter)
+                )
+            )
+        }
     }.stateIn(
         scope = viewModelScope,
         started = SharingStarted.WhileSubscribed(5_000),
